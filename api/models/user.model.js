357d79b493c08ef768fbee2a88846f97c596a1cf
Jerome Doran <disneyfan9184@gmail.com>
2023-11-05 12:23:01 -0500

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import bcryptjs from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username must be at least one character long.'],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter a valid email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter a password'],
+      minLength: [6, 'Your password must be at least 6 characters'],
+    },
+    profilePicture: {
+      type: String,
+      default:
+        'https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg',
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  this.password = await bcryptjs.hash(this.password, 10);
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
