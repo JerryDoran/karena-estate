1a40e78a26f2fcfd46a35902109077914779d051
Jerome Doran <disneyfan9184@gmail.com>
2023-11-05 12:14:09 -0500

    set up connection to database

--- api/index.js
+++ api/index.js
@@ -1,16 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to the database!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('route', (req, res) => {
-  
-});
+app.get('route', (req, res) => {});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
