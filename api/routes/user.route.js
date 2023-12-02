195a863266bc6de782d01fa023d106f81a2d647a
Jerome Doran <disneyfan9184@gmail.com>
2023-12-02 11:58:08 -0500

    create user listing api endpoint

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,10 @@
 import express from 'express';
 
-import { updateUser, deleteUser } from '../controllers/user.controller.js';
+import {
+  updateUser,
+  deleteUser,
+  getUserListings,
+} from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +12,6 @@ const router = express.Router();
 // router.get('/', getUser);
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
